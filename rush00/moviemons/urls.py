940bc121a0cdf8621cb8acc84bb6dc8dd3618d6c
Zhang WENG <zweng@e3r9p18.42.fr>
2019-10-19 16:16:04 +0200

    [first]

--- rush00/moviemons/urls.py
+++ rush00/moviemons/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path, re_path
+from django.urls import path, url
 
 from . import views
 
 app_name='moviemons'
 urlpatterns = [
-    # re_path('', views.),
+	url(r'^options/load_game', views.OptionsLoad),
+	url(r'^options/save_game', views.OptionsSave),
+	url(r'^battle/(?P<moviemon_id>[-\w]+)/$', views.Battle),
+	url(r'^worldmap', views.Worldmap),
+	url(r'^options', views.Options),
+	url(r'^moviedex/(?P<moviemon_id>[-\w]+)$', views.MoviedexDetail),
+	url(r'^moviedex', views.Moviedex),
+	url(r'^$', views.new),
 ]
