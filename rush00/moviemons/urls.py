9095d05b9153da4bbed3d525efff7a33d256c1a2
Zhang WENG <zweng@e3r9p18.42.fr>
2019-10-19 17:57:06 +0200

    tmp

--- rush00/moviemons/urls.py
+++ rush00/moviemons/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	# url(r'^options/load_game', views.OptionsLoad),
 	# url(r'^options/save_game', views.OptionsSave),
 	# url(r'^battle/(?P<moviemon_id>[-\w]+)/$', views.Battle),
-	# url(r'^worldmap', views.Worldmap),
+	url(r'^worldmap', views.Worldmap),
 	# url(r'^options', views.Options),
 	# url(r'^moviedex/(?P<moviemon_id>[-\w]+)$', views.MoviedexDetail),
 	# url(r'^moviedex', views.Moviedex),
